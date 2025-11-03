57af7acea0fcc962a4dd8d360fa4850decf03151
Gergely Kocsis <kocsisger@example.com>
2025-11-03 08:29:37 +0100

    Initial commit

---
+++ src/main/java/en/unideb/inf/SpringDataFX/SpringDataFxApplication.java
@@ -0,0 +1,13 @@
+package en.unideb.inf.SpringDataFX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataFxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataFxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/en/unideb/inf/SpringDataFX/SpringDataFxApplicationTests.java
@@ -0,0 +1,13 @@
+package en.unideb.inf.SpringDataFX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataFxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
